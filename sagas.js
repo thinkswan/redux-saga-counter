56ce9e1a255cc87550eb8d43080d4a68fbd88260
Graham Swan <thinkswan@gmail.com>
2018-07-11 11:54:21 +0200

    Add async increment saga

--- sagas.js
+++ sagas.js
@@ -1,3 +1,26 @@
-export function* helloSaga () {
+import { delay } from 'redux-saga'
+import { put, takeEvery, all } from 'redux-saga/effects'
+
+// Worker saga: Log a message to the console
+function* helloSaga () {
   console.log('Hello sagas!')
+}
+
+// Worker saga: Preform the async increment task
+function* incrementAsync () {
+  yield delay(1000)
+  yield put({ type: 'INCREMENT' })
+}
+
+// Watcher saga: spawn a new incrementAsync task for each INCREMENT_ASYNC action
+function* watchIncrementAsync () {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+// Export a single entry point to start all sagas
+export default function* rootSaga () {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ])
 }
